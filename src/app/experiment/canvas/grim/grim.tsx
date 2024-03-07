28da56b9cd7e3bcf769e887d2f10d6ac276f7baf
shindaeho <sdh077@naver.com>
2024-03-07 19:12:24 +0900

    grim

--- src/app/experiment/canvas/grim/grim.tsx
+++ src/app/experiment/canvas/grim/grim.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import clsx from "clsx";
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Grim() {
   const canvasGrim = useRef<HTMLCanvasElement>()
+  let mouse = { x: 0, y: 0 }
   let ctx: CanvasRenderingContext2D
   const colors = ['black', 'white', 'red', 'yellow', 'orange', 'green', 'blue', 'navy', 'purple']
   const [choiceColor, setColor] = useState('black');
@@ -12,22 +13,26 @@ export default function Grim() {
 
   const painting = (event) => {
     if (isPainting) {
-      console.log(isPainting)
       ctx = canvasGrim.current.getContext("2d");
       event.preventDefault();   // drag 방지
       event.stopPropagation();  // drag 방지
 
-      ctx.strokeStyle = choiceColor
-      ctx.lineWidth = 2.5
-      ctx.lineJoin = 'round'
       const x = event.pageX - canvasGrim.current.offsetLeft
       const y = event.pageY - canvasGrim.current.offsetTop
+
+      if (mouse.x === 0 && mouse.y === 0) return mouse = { x, y }
+
+      ctx.strokeStyle = choiceColor
+      console.log(ctx.strokeStyle, choiceColor)
+      ctx.lineWidth = 2.5
+      ctx.lineJoin = 'round'
+
       ctx.beginPath()
       ctx.moveTo(x, y)
-      ctx.lineTo(x + 1, y + 1);
+      ctx.lineTo(mouse.x, mouse.y);
       ctx.closePath()
       ctx.stroke()
-      // setMouse({ x, y })
+      mouse = { x, y }
     }
   }
   const resize = () => {
@@ -40,20 +45,25 @@ export default function Grim() {
 
     ctx.scale(pixelRatio, pixelRatio);
   }
-  const handlePaint = useCallback((event: any, onOff: boolean) => {
+  const handlePaint = (event: any, onOff: boolean) => {
+    mouse = { x: 0, y: 0 }
     isPainting = onOff
-  }, [])
+  }
 
   useEffect(() => {
     if (!canvasGrim.current) return
     ctx = canvasGrim.current.getContext("2d");
     window.addEventListener("resize", resize.bind(this), false);
     resize();
+  }, [canvasGrim.current])
+
+  useEffect(() => {
+    if (!canvasGrim.current) return
     canvasGrim.current.addEventListener("mousedown", (event) => handlePaint(event, true))
     canvasGrim.current.addEventListener("mouseup", (event) => handlePaint(event, false))
     canvasGrim.current.addEventListener("mousemove", (event) => painting(event))
     canvasGrim.current.addEventListener("mouseleave", (event) => handlePaint(event, false))
-  }, [canvasGrim.current])
+  }, [canvasGrim.current, choiceColor])
 
   return (
     <div className="bg-slate-100 flex text-black">
--- src/app/experiment/layout.tsx
+++ src/app/experiment/layout.tsx
@@ -4,16 +4,13 @@ import { Container } from '@components/Container'
 import './motion.css'
 
 export default function layout({ params: { id }, children }: { params: { id: string }, children: React.ReactNode }) {
-    const navs = ['motion', 'dnd', 'chat']
+    const navs = ['motion', 'dnd', 'canvas', 'chat']
     return (
         <>
             <Container className='mt-4'>
                 <DropdownComponent items={navs} />
             </Container>
             <div
-            //  className='absolute left-1/2 top-1/4' style={{
-            //     transform: 'translateX(-50%)'
-            // }}
             >
                 {children}
             </div>
